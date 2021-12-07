00387786688e7d00ccd09fbdc164e5b7724c53cc
Evatitan <yinguihua2014@gmail.com>
2021-12-07 22:03:23 +0100

    add style for travel component

--- src/pages/Content/Travel/index.jsx
+++ src/pages/Content/Travel/index.jsx
@@ -11,119 +11,10 @@ export default class Travel extends Component {
 		this.state = {
 			lng: -3.7,
 			lat: 40.41,
-			zoom: 1
+			zoom: 2
 		};
 		this.mapContainer = React.createRef();
 	}
-
-	// componentDidMountMarker() {
-	// 	const { lng, lat, zoom } = this.state;
-
-	// 	const map = new mapboxgl.Map({
-	// 		container: this.mapContainer.current,
-	// 		style: 'mapbox://styles/mapbox/streets-v11',
-	// 		center: [ lng, lat ],
-	// 		zoom: zoom
-	// 	});
-
-	// 	map.on('move', () => {
-	// 		this.setState({
-	// 			lng: map.getCenter().lng.toFixed(4),
-	// 			lat: map.getCenter().lat.toFixed(4),
-	// 			zoom: map.getZoom().toFixed(2)
-	// 		});
-	// 	});
-
-	// 	map.on('load', () => {
-	// 		// Add an image to use as a custom marker
-	// 		map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', (error, image) => {
-	// 			if (error) throw error;
-	// 			map.addImage('custom-marker', image);
-	// 			// Add a GeoJSON source with 2 points
-	// 			map.addSource('points', {
-	// 				type: 'geojson',
-	// 				data: {
-	// 					type: 'FeatureCollection',
-	// 					features: [
-	// 						{
-	// 							type: 'Feature',
-	// 							geometry: {
-	// 								type: 'Point',
-	// 								coordinates: [ 120.16, 30.29 ]
-	// 							},
-	// 							properties: {
-	// 								title: 'HangZhou'
-	// 							}
-	// 						},
-	// 						{
-	// 							type: 'Feature',
-	// 							geometry: {
-	// 								type: 'Point',
-	// 								coordinates: [ 116.38, 39.9 ]
-	// 							},
-	// 							properties: {
-	// 								title: 'BeiJing'
-	// 							}
-	// 						},
-	// 						{
-	// 							type: 'Feature',
-	// 							geometry: {
-	// 								type: 'Point',
-	// 								coordinates: [ -3.7, 40.41 ]
-	// 							},
-	// 							properties: {
-	// 								title: 'Madrid'
-	// 							}
-	// 						},
-	// 						{
-	// 							type: 'Feature',
-	// 							geometry: {
-	// 								type: 'Point',
-	// 								coordinates: [ 6.08, 52.51 ]
-	// 							},
-	// 							properties: {
-	// 								title: 'Zwolle'
-	// 							}
-	// 						},
-	// 						{
-	// 							type: 'Feature',
-	// 							geometry: {
-	// 								type: 'Point',
-	// 								coordinates: [ 6.08, 52.51 ]
-	// 							},
-	// 							properties: {
-	// 								title: 'Zwolle'
-	// 							}
-	// 						},
-	// 						{
-	// 							type: 'Feature',
-	// 							geometry: {
-	// 								type: 'Point',
-	// 								coordinates: [ 2.34, 48.85 ]
-	// 							},
-	// 							properties: {
-	// 								title: 'Paris'
-	// 							}
-	// 						}
-	// 					]
-	// 				}
-	// 			});
-
-	// 			map.addLayer({
-	// 				id: 'points',
-	// 				type: 'symbol',
-	// 				source: 'points',
-	// 				layout: {
-	// 					'icon-image': 'custom-marker',
-	// 					'text-field': [ 'get', 'title' ],
-	// 					'text-font': [ 'Open Sans Semibold', 'Arial Unicode MS Bold' ],
-	// 					'text-offset': [ 0, 1.25 ],
-	// 					'text-anchor': 'top'
-	// 				}
-	// 			});
-	// 		});
-	// 	});
-	// }
 
 	componentDidMount() {
 		const { lng, lat, zoom } = this.state;
@@ -196,14 +87,10 @@ export default class Travel extends Component {
 	}
 
 	render() {
-		const { lng, lat, zoom } = this.state;
 		return (
 			<div>
-				{/* <div className="sidebar">
-					Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-				</div> */}
 				<div ref={this.mapContainer} className="map-container" />
-				<div className="columns">
+				<div className="columns travel-page">
 					<div className="column">
 						<h2>My Travel Map</h2>
 						<p className="description">
