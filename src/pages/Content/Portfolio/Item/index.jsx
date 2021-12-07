220cabb29c7550b8c8a05f7106497aa95b53b218
Evatitan <yinguihua2014@gmail.com>
2021-12-07 20:50:41 +0100

    creat component Portfolio

--- src/pages/Content/Portfolio/Item/index.jsx
+++ src/pages/Content/Portfolio/Item/index.jsx
@@ -1,15 +1,48 @@
-import React,{Component} from "react";
-//import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLaptop } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-import "./index.css";
+import './index.css';
+library.add(fab, faLaptop);
 
-export default class Item extends Component{
-    render (){
+export default class Item extends Component {
+	render() {
+		const { name, description } = this.props;
+		return (
+			<div className="column is-one-fifth">
+				<div className="card">
+					<div className="card-image">
+						<figure className="image">
+							<img src="https://bulma.io/images/placeholders/1280x960.png" alt="" />
+						</figure>
+					</div>
 
-        return (
-          <div>
-            
-          </div>
-        )
-    }
+					<div className="card-content">
+						<p>
+							{name} <br /> {description}
+						</p>
+					</div>
+
+					<footer className="card-footer">
+						<p className="card-footer-item">
+							<span>
+								<a href="#">
+									<FontAwesomeIcon icon={faLaptop} />
+								</a>
+							</span>
+						</p>
+						<p className="card-footer-item">
+							<span>
+								<a href="https://github.com/evatitan/perfil">
+									<FontAwesomeIcon icon={[ 'fab', 'github' ]} />
+								</a>
+							</span>
+						</p>
+					</footer>
+				</div>
+			</div>
+		);
+	}
 }
--- src/pages/Content/Portfolio/index.jsx
+++ src/pages/Content/Portfolio/index.jsx
@@ -1,15 +1,23 @@
-import React,{Component} from "react";
+import React, { Component } from 'react';
+import Item from './Item';
+import './index.css';
 
-export default class Portfolio extends Component{
-    
-
-    render (){
-
-        return (
-            <div>
-             
-            </div>
-
-        )
-    }
+export default class Portfolio extends Component {
+	state = {
+		projects: [
+			{ id: '001', name: 'Resume', description: 'react' },
+			{ id: '002', name: 'TodoList', description: 'react' },
+			{ id: '003', name: 'Github User Search', description: 'react' }
+		]
+	};
+	render() {
+		const { projects } = this.state;
+		return (
+			<div className="columns">
+				{projects.map((projectObj) => {
+					return <Item key={projectObj.id} {...projectObj} />;
+				})}
+			</div>
+		);
+	}
 }
