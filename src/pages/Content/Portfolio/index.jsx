758d8c8c857ca6d15753b47f890b0493566abe37
Evatitan <yinguihua2014@gmail.com>
2021-12-17 11:33:44 +0100

    Error boundary for component item in the component Portfolio

--- src/pages/Content/Portfolio/index.jsx
+++ src/pages/Content/Portfolio/index.jsx
@@ -11,15 +11,27 @@ export default class Portfolio extends Component {
 			{ id: '001', name: 'Resume', description: 'React', img: evaResume },
 			{ id: '002', name: 'scoreGame', description: 'JavaScript', img: scoreGame },
 			{ id: '003', name: 'urTurn', description: 'Angular', img: urTurn }
-		]
+		],
+		hasError: ''
 	};
+
+	static getDerivedStateFromError(error) {
+		return { hasError: error };
+	}
+	componentDidCatch(error) {
+		console.log(error);
+	}
 	render() {
 		const { projects } = this.state;
 		return (
 			<div className="columns page-portfolio">
-				{projects.map((projectObj) => {
-					return <Item key={projectObj.id} {...projectObj} />;
-				})}
+				{this.state.hasError ? (
+					<h2>The internet is bad, please try it later.</h2>
+				) : (
+					projects.map((projectObj) => {
+						return <Item key={projectObj.id} {...projectObj} />;
+					})
+				)}
 			</div>
 		);
 	}
