9f8765b97dcc4e1b896098520a8a3c6d6dce1ab3
Evatitan <yinguihua2014@gmail.com>
2022-01-03 18:09:30 +0100

    css for About

--- src/pages/Content/About/index.jsx
+++ src/pages/Content/About/index.jsx
@@ -8,28 +8,34 @@ import eva from './eva.jpeg';
 export default class About extends Component {
 	render() {
 		return (
-			<div>
-				<div className="columns about-page">
-					<div className="column about">
-						<h2>About Me</h2>
-						<p>
-							Hi &nbsp;&nbsp;&nbsp;:) <br />
-							I'm a Full Stack Web Developer that like to try more things in the life.
-						</p>
-					</div>
-
-					<div className="column ">
-						<img className="column-img about-img" src={eva} alt="eva" />
-					</div>
-
-					<div className="column details">
-						<h2>Contact Details</h2>
-						<p>
-							<FontAwesomeIcon icon={faAddressCard} /> Guihua Yin / Eva <br />
-							<FontAwesomeIcon icon={faHome} /> Spain <br />
-							<FontAwesomeIcon icon={faGlobeAsia} /> Chinese <br />
-							<FontAwesomeIcon icon={faEnvelopeOpenText} /> yinguihua2014@gmail.com
-						</p>
+			<div className="about-page">
+				<div className="tile is-vertical is-10 titleContainer">
+					<div className="tile">
+						<div className="tile is-parent is-vertical">
+							<article className="tile is-child notification">
+								<p className="title">About Me</p>
+								<p className="subtitle">
+									Hi &nbsp;&nbsp;&nbsp;:) <br />
+									I'm a Full Stack Web Developer, I'd like to do different things in the life.
+								</p>
+							</article>
+							<article className="tile is-child notification">
+								<p className="title">Contact Details</p>
+								<p className="subtitle">
+									<FontAwesomeIcon icon={faAddressCard} /> Guihua Yin / Eva <br />
+									<FontAwesomeIcon icon={faHome} /> Spain <br />
+									<FontAwesomeIcon icon={faGlobeAsia} /> Chinese <br />
+									<FontAwesomeIcon icon={faEnvelopeOpenText} /> yinguihua2014@gmail.com
+								</p>
+							</article>
+						</div>
+						<div className="tile is-parent">
+							<article className="tile is-child notification">
+								<figure className="image is-4by3">
+									<img src={eva} alt="eva" />
+								</figure>
+							</article>
+						</div>
 					</div>
 				</div>
 			</div>
